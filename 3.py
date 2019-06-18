e42d4eb62db12d8865f7366e144017b6a4a5cd29
INDHU10 <51222588+INDHU10@users.noreply.github.com>
2019-06-18 16:48:19 +0530

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,8 @@
+wy=input()
+if(wy=='a'or wy=='e' or wy=='i' or wy=='o' or wy=='u'or wy=='A' or wy=='E'or wy=='I' or wy=='O' or wy=='U'):
+  print("Vowel")
+elif(wy=='b' or wy=='B'or wy=='c'or wy=='C'or wy=='d'or wy=='D'or wy=='F'or wy=='f'or wy=='g'or wy=='G'or wy=='h'or wy=='H'or wy=='J'or wy=='j'or wy=='k'or wy=='K'or wy=='l'or wy=='L'or wy=='M'or wy=='m'or wy=='n'or wy=='N'or wy==wy=='P'or wy=='p'or wy=='q'orwy=='Q'or wy=='R'or wy=='r'or wy=='s'or wy=='S'or wy=='T'or wy=='t'or wy=='v'or wy=='V'or wy=='w'or wy=='W'or wy=='X'or wy=='x'or wy=='y'or wy=='Y'or wy=='Z'or wy=='z'):
+  print("Consonant")
+else:
+  print("invalid")
+  
